f2b5e5ea8464d3b45804c59499e7c8d54d743585
Maria Eduarda Araújo de Moraes <109601939+meaeduarda@users.noreply.github.com>
2026-03-06 09:55:34 -0300

    Update pricing-calculator.js

--- assets/js/pricing-calculator.js
+++ assets/js/pricing-calculator.js
@@ -15,7 +15,7 @@ class PricingCalculator {
             },
             pro: {
                 name: "Cronos Pro",
-                originalPrice: 4548.00,
+                originalPrice: 4932.00,
                 discountPercentage: 10,
                 months: 12,
                 icon: "fa-crown"
